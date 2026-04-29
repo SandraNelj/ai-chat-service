56a535b665dbef7a6d9eceb78eefbc8b96c2e0a7
Sandra <229708855+SandraNelj@users.noreply.github.com>
2026-04-29 11:13:47 +0200

    Initial setup with dependencies and models

---
+++ src/main/java/org/example/aichatservice/model/ChatResponse.java
@@ -0,0 +1,13 @@
+package org.example.aichatservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatResponse {
+    private String reply;
+    private String personality;
+}

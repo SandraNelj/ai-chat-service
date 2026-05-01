f216c4d7e1ffd8bfa9d83d01ff078c41c817c010
Sandra <229708855+SandraNelj@users.noreply.github.com>
2026-05-01 09:39:39 +0200

    feat: add request/response models and API DTOs

---
+++ src/main/java/org/example/aichatservice/dto/MessageDto.java
@@ -0,0 +1,12 @@
+package org.example.aichatservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageDto {
+
+    private String role;
+    private String content;
+}
